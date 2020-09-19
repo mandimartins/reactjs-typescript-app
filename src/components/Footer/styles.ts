4581cdfdd9001b776d4e982bf6c1c56d4bbae3e1
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-09-19 13:44:53 -0300

    Delete footer

--- src/components/Footer/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.a`
-  margin-top: 1rem;
-  font-size: 1.4rem;
-  color: ${(props) => props.theme.colors.tertiary};
-  display: flex;
-  justify-content: center;
-`;
